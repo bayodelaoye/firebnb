97a42aa4862db244d10d4c2ee74627f622ab39c2
Bayode <laoye.bayode@gmail.com>
2024-05-09 19:58:54 -0400

    Changed users file

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -33,32 +33,6 @@ const validateSignup = [
   handleValidationErrors,
 ];
 
-// router.post("/", validateSignup, async (req, res) => {
-//   const { firstName, lastName, email, username, password } = req.body;
-//   const hashedPassword = bcrypt.hashSync(password);
-//   const user = await User.create({
-//     firstName,
-//     lastName,
-//     email,
-//     username,
-//     hashedPassword,
-//   });
-
-//   const safeUser = {
-//     id: user.id,
-//     firstName: user.firstName,
-//     lastName: user.lastName,
-//     email: user.email,
-//     username: user.username,
-//   };
-
-//   await setTokenCookie(res, safeUser);
-
-//   return res.json({
-//     user: safeUser,
-//   });
-// });
-
 router.post("/", async (req, res) => {
   const error = {
     message: {},
