ef151eb4a492e02e2ad203b50b5d54c2fd67da3f
Bayode <laoye.bayode@gmail.com>
2024-06-07 15:13:27 -0400

    Working on bug

--- frontend/src/components/Spots/SpotsIndex.jsx
+++ frontend/src/components/Spots/SpotsIndex.jsx
@@ -2,10 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllSpots } from "../../store/spotReducer";
 import { useState } from "react";
-// import SpotIndexItem from "./SpotIndexItem";
+import SpotIndexItem from "./SpotIndexItem";
 import "./SpotsIndex.css";
-import { FaStar } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 const SpotsIndex = () => {
   const spotsObj = useSelector((state) => state.spots.allSpots);
@@ -16,6 +14,7 @@ const SpotsIndex = () => {
     // do nothing again 1hjbhjb
   } else {
     spots = Object.values(spotsObj);
+    console.log(spots[0].avgRating);
   }
   const dispatch = useDispatch();
 
@@ -23,39 +22,13 @@ const SpotsIndex = () => {
     dispatch(getAllSpots()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  console.log(spots);
   return (
     <div>
       {isLoaded ? (
         <div className="spots-container">
           {spots.map((spot) => {
             console.log(spot);
-            // return <SpotIndexItem spot={spot} />;
-            <div className="spot-container">
-              <Link to={`/spots/${spot.id}`}>
-                <div className="spot-image-container">
-                  <img src={spot.previewImage} />
-                </div>
-                <div className="tooltip">
-                  <span className="tooltiptext">{spot.name}</span>
-                </div>
-                <div className="spot-text-container">
-                  <h2>{spot.name}</h2>
-                  <div className="spot-location-rating-container">
-                    <p>
-                      {spot.city}, {spot.state}
-                    </p>
-                    <p>
-                      <FaStar />{" "}
-                      {spot.avgRating
-                        ? Number(spot.avgRating).toFixed(2)
-                        : "New"}
-                    </p>
-                  </div>
-                  <p>${spot.price} night</p>
-                </div>
-              </Link>
-            </div>;
+            return <SpotIndexItem spot={spot} key={spot.id} />;
           })}
         </div>
       ) : (
